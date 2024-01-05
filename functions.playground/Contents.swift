7969b6ef774067b2a1cbb288ab115254ac6b9e45
Alexander Nitiola <cre8tor.alexander@gmail.com>
2024-01-05 14:43:24 +0100

    completed the function lecture

--- functions.playground/Contents.swift
+++ functions.playground/Contents.swift
@@ -1,3 +1,9 @@
+func noArgsAndNoReturnValue() -> Void {
+    "I don't know what I'm doing"
+}
+
+noArgsAndNoReturnValue()
+
 func remove_character(_ c: Character, from s: inout String) -> Int {
     var how_many = 0;
     
@@ -18,3 +24,29 @@ for (idx, value) in age.enumerated() {
     print("Value: \(value)");
     print("idx: \(idx)");
 }
+
+@discardableResult
+func customAdd(_ lhs: Int, _ rhs: Int) -> Int {
+    return lhs + rhs
+}
+
+customAdd(23, 40)
+
+// function hosting another function
+
+func doSomethingComplecated(with value: Int) -> Int {
+    func mainLogic(value: Int) -> Int {
+        value + 2
+    }
+    
+    return mainLogic(value: value + 3)
+}
+
+doSomethingComplecated(with: 30)
+
+func functionWithDefaultValue(with value: Int = 20) -> Int {
+    value + 30
+}
+
+
+functionWithDefaultValue()
